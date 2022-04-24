9e46893b399e98d38384e680797cdf0cbcc1cd28
Ahmed-ata112 <ahmedatta2108@gmail.com>
2022-04-24 10:40:03 +0200

    first lines in RR

--- code/scheduler.c
+++ code/scheduler.c
@@ -1,8 +1,14 @@
 #include "headers.h"
 #include "hashmap.h"
 #include "circular_queue.h"
+enum state{
+    READY=1,
+    RUNNING=2,
+};
+
 
 struct PCB {
+    int id; //this is the key in the hashmap
     int pid;
     int arrival_time;
     int priority;
@@ -16,7 +22,7 @@ struct PCB {
 int process_compare(const void *a, const void *b, void *udata) {
     const struct PCB *process_a = a;
     const struct PCB *process_b = b;
-    return (process_a->pid == process_b->pid ? 0 : 1);
+    return (process_a->id == process_b->id ? 0 : 1);
 }
 
 bool process_iter(const void *item, void *udata) {
@@ -34,7 +40,7 @@ uint64_t process_hash(const void *item, uint64_t seed0, uint64_t seed1) {
 
 // TODO init this in the main of the scheduler
 struct hashmap *process_table;
-int process_msgqueue;
+int process_msg_queue;
 
 // when process generator tells us that there is no more to come
 // set this to false
@@ -65,14 +71,23 @@ void RR() {
         struct msqid_ds buf;
         int num_messages;
 
-        msgctl(process_msgqueue, IPC_STAT, &buf);
+        msgctl(process_msg_queue, IPC_STAT, &buf);
         num_messages = buf.msg_qnum;
 
         while (num_messages > 0) {
             // while still a process in the queue
             // take it out
             // add it to both the RRqueue and its PCB to the processTable
+            struct process_struct message;
+            msgrcv(process_msg_queue,&message,sizeof(message) - sizeof(message.mtype),0,!IPC_NOWAIT );
 
+            //you have that struct Now
+            struct PCB pcb;
+            pcb.id = message.id;
+            pcb.priority = message.priority;
+            pcb.arrival_time = message.arrival;
+            pcb.remaining_time = message.runtime; // at the beginning
+            hashmap_set(process_table,&pcb); // this copies the content of the struct
 
             num_messages--;
         }
