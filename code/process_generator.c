9bdfeb31fb5d16cabd2e4d55eab6b11940358924
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-04-29 00:56:08 +0200

    solve errors

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,4 +1,5 @@
 #include "headers.h"
+#include <string.h>
 #define FILE_NAME_LENGTH 100
 
 //message queue id
@@ -7,7 +8,7 @@ int msgq_id;
 void clearResources(int);
 int NumOfProcesses (FILE* file , char FileName[]);
 void ReadProcessesData (FILE* file, struct process_struct Processes[],int ProcessesNum);
-void Create_Scheduler_Clk(int& sch_pid, int& clk_pid);
+void Create_Scheduler_Clk(int* sch_pid, int* clk_pid);
 
 int main(int argc, char * argv[])
 {
@@ -17,7 +18,7 @@ int main(int argc, char * argv[])
     char FileName[FILE_NAME_LENGTH];
     strcpy(FileName,argv[1]);
     FILE* file = fopen(FileName,"r");
-    int ProcessesNum = NumOfProcesses(file);
+    int ProcessesNum = NumOfProcesses(file,FileName);
     //make an array of processes and store data of each process in it
     struct process_struct Processes[ProcessesNum];
     ReadProcessesData(file,Processes,ProcessesNum);
@@ -25,7 +26,7 @@ int main(int argc, char * argv[])
 
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
     struct chosen_algorithm Initializer;
-    Initializer.algo = argv[2];
+    Initializer.algo = (short)atoi(argv[2]);
     Initializer.arg = 0;
     if (argc == 4)
     {
@@ -36,7 +37,7 @@ int main(int argc, char * argv[])
     // 3. Initiate and create the scheduler and clock processes.
     //using forking
     int sch_pid,clk_pid,stat_loc;
-    Create_Scheduler_Clk(sch_pid,clk_pid);
+    Create_Scheduler_Clk(&sch_pid,&clk_pid);
     
     // 4. Use this function after creating the clock process to initialize clock
     initClk();
@@ -47,11 +48,11 @@ int main(int argc, char * argv[])
     // 5. Create a data structure for processes and provide it with its parameters.
     //create a message queue
     //create the queue id
-    key_t qid = ftok(FileName,"q");
+    key_t qid = ftok("keyfile",65);
     msgq_id = msgget(qid,0666|IPC_CREAT);
     if (msgq_id == -1)
     {
-        perror("error has been occured in the message queue\n");
+        printf("error has been occured in the message queue\n");
         exit(-1);
     }
 
@@ -60,7 +61,7 @@ int main(int argc, char * argv[])
     int send_val = msgsnd(msgq_id,&Initializer,sizeof(Initializer)-sizeof(Initializer.mtype),!IPC_NOWAIT);
     if (send_val == -1)
     {
-        perror("error has been occured in scheduler initation operation\n");
+        printf("error has been occured in scheduler initation operation\n");
         exit(-1);
     }
     //secondly, sending processes in the appropiate time
@@ -70,10 +71,10 @@ int main(int argc, char * argv[])
         if (getClk()>=Processes[ProcessIterator].arrival)
         {
             //send the process to the schedular
-            send_val = msgsnd(msgq_id,&Processes[ProcessIterator],sizeof(Processes[ProcessIterator].ProcessData),!IPC_NOWAIT);
+            send_val = msgsnd(msgq_id,&Processes[ProcessIterator],sizeof(Processes[ProcessIterator])-sizeof(Processes[ProcessIterator].mtype),!IPC_NOWAIT);
             if (send_val == -1)
             {
-                perror("error has been occured while sending a process number %d to the schedular\n",ProcessIterator);
+                printf("error has been occured while sending a process number %d to the schedular\n",ProcessIterator);
             }
             ProcessIterator++;
         }
@@ -101,7 +102,7 @@ int NumOfProcesses (FILE* file,char FileName[])
 {
     if (file == NULL)
     {
-        perror("the file %s does not exist",FileName);
+        printf("the file %s does not exist",FileName);
         exit(-1);
     }
     //number of lines in the file determines number of process
@@ -140,25 +141,25 @@ void ReadProcessesData(FILE* file, struct process_struct Processes[],int Process
         fscanf(file,"%d",&Processes[i].priority);
     }
 }
-void Create_Scheduler_Clk(int& sch_pid, int& clk_pid)
+void Create_Scheduler_Clk(int* sch_pid, int* clk_pid)
 {
-    sch_pid = fork();
-    if (sch_pid == -1)
+    *sch_pid = fork();
+    if (*sch_pid == -1)
     {
-        perror("error has been occured while initiating the scheduler\n");
+        printf("error has been occured while initiating the scheduler\n");
         exit(-1);
     }
-    if (sch_pid == 0)
+    if (*sch_pid == 0)
     {
         execl("./scheduler.out","./scheduler.out",NULL);
     }
-    clk_pid = fork();
-    if (clk_pid == -1)
+    *clk_pid = fork();
+    if (*clk_pid == -1)
     {
-        perror("error has been occured while initiating the clock\n");
+        printf("error has been occured while initiating the clock\n");
         exit(-1);
     }
-    if (clk_pid == 0)
+    if (*clk_pid == 0)
     {
         execl("./clk.out","./clk.out",NULL);
     }
