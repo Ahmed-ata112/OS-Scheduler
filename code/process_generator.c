3264ba44ed1054328086629c53557a1872d4d1c1
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-04-28 06:27:17 +0200

    Update Process Generator.c
    need to handle busy waiting after sending

--- code/process_generator.c
+++ code/process_generator.c
@@ -1,71 +1,189 @@
 #include "headers.h"
+#define FILE_NAME_LENGTH 100
+
+struct Process
+{
+    long mtype;
+    //==============================
+    //index ---->    meaning      ||
+    //  0   ---->    ID           ||
+    //  1   ---->    arrival time ||
+    //  2   ---->    runtime      ||
+    //  3   ---->    priority     ||
+    //============================== 
+    int ProcessData[4];
+};
+
+struct SchedulerInit
+{
+    long mtype;
+    //=====================================
+    //index ---->    meaning             ||
+    //  0   ---->    Number Of Processes ||
+    //  1   ---->    scheduling Algorithm||
+    //  2   ---->    quantum if any      ||
+    //=====================================
+    int Info[3]; 
+};
+
+//message queue id
+int msgq_id;
 
 void clearResources(int);
+int NumOfProcesses (FILE* file , char FileName[]);
+void ReadProcessesData (FILE* file, struct Process Processes[],int ProcessesNum);
+void Create_Scheduler_Clk(int& sch_pid, int& clk_pid);
 
-int process_msg_queue;
-
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[])
+{
     signal(SIGINT, clearResources);
-    // TODO Initialization
+    
     // 1. Read the input files.
+    char FileName[FILE_NAME_LENGTH];
+    strcpy(FileName,argv[1]);
+    FILE* file = fopen(FileName,"r");
+    int ProcessesNum = NumOfProcesses(file);
+    //make an array of processes and store data of each process in it
+    struct Process Processes[ProcessesNum];
+    ReadProcessesData(file,Processes,ProcessesNum);
+    fclose(file);
+
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    int Algorithm = argv[2];
+    int quantum = 0;
+    if (argc == 4)
+    {
+        quantum = atoi(argv[3]);
+    }
+
     // 3. Initiate and create the scheduler and clock processes.
+    //using forking
+    int sch_pid,clk_pid;
+    Create_Scheduler_Clk(sch_pid,clk_pid);
+    
     // 4. Use this function after creating the clock process to initialize clock
-    int sch_pid = fork();
-    if (sch_pid == 0) {
-        execl("./scheduler.out", "./scheduler.out", NULL);
-    }
-    int i = fork();
-    if (i == 0) {
-        execl("./clk.out", "./clk.out", NULL);
-    }
-
     initClk();
-    // To get time use this
-    int x = getClk();
 
-    printf("current time is %d\n", x);
+    // To get time use this getClk();
+    
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
-    // 6. Send the information to the scheduler at the appropriate time.
-    // 7. Clear clock resources
-
-    /**
-     * @atta
-     * TODO: Change THESE
-     * @note just for debugging
-     */
-
-
-    int key_id = ftok("keyfile", 65);
-    process_msg_queue = msgget(key_id, 0666 | IPC_CREAT);
-    struct chosen_algorithm coming;
-    coming.algo = 1; // RR
-    coming.arg = 1;  // q
-    coming.mtype = ALGO_TYPE;
-    msgsnd(process_msg_queue, &coming, sizeof(coming) - sizeof(coming.mtype),
-           !IPC_NOWAIT);
-
-    // I will send some Process to simulate this
-    struct process_struct pp;
-    for (int i = 0; i < 3; ++i) {
-        pp.mtype = PROC_TYPE;
-        pp.runtime = 1;
-        pp.priority = 2;
-        pp.arrival = getClk();
-        pp.id = i;
-        msgsnd(process_msg_queue, &pp, sizeof(pp) - sizeof(pp.mtype),
-               !IPC_NOWAIT);
-
+    //create a message queue
+    //create the queue id
+    key_t qid = ftok(FileName,"q");
+    msgq_id = msgget(qid,0666|IPC_CREAT);
+    if (msgq_id == -1)
+    {
+        perror("error has been occured in the message queue\n");
+        exit(-1);
     }
-    sleep(1);
 
-    kill(sch_pid, SIGUSR1); //
-    sleep(100);
+    // 6. Send the information to the scheduler at the appropriate time.
+    //firtly, initialize the scheduler
+    struct SchedularInit Initializer;
+    Initializer.Info[0] = ProcessesNum;
+    Initializer.Info[1] = Algorithm;
+    Initializer.Info[2] = quantum;
+    Initializer.mtype = 1;  //dummy
+    //send the initiator struct to the scheduler
+    int send_val = msgsnd(msgq_id,&Initializer,sizeof(Initializer.Info),!IPC_NOWAIT);
+    if (send_val == -1)
+    {
+        perror("error has been occured in scheduler initation operation\n");
+        exit(-1);
+    }
+    //secondly, sending processes in the appropiate time
+    int ProcessIterator = 0;
+    while(1)
+    {
+        while (getClk()>=Processes->ProcessData[1] && ProcessIterator < ProcessesNum)
+        {
+            //send the process to the schedular
+            send_val = msgsnd(msgq_id,&Processes[ProcessIterator],sizeof(Processes[ProcessIterator].ProcessData),!IPC_NOWAIT);
+            if (send_val == -1)
+            {
+                perror("error has been occured while sending a process number %d to the schedular\n",ProcessIterator);
+            }
+            ProcessIterator++;
+        }   
+    }
+    
+    // 7. Clear clock resources
     destroyClk(true);
 }
 
-void clearResources(int signum) {
-    // TODO Clears all resources in case of interruption
-    msgctl(process_msg_queue, IPC_RMID, 0);
+void clearResources(int signum)
+{
+    //TODO Clears all resources in case of interruption
+    msgctl(msgq_id,IPC_RMID,(struct msqid_ds*)0);
+    kill(getpgrp(),SIGKILL);
+    //is it required to clear clock resources also????
+    signal(SIGINT,clearResources);
+}
+
+//remember to add the file name in it
+int NumOfProcesses (FILE* file,char FileName[])
+{
+    if (file == NULL)
+    {
+        perror("the file %s does not exist",FileName);
+        exit(-1);
+    }
+    //number of lines in the file determines number of process
+    //note : do not count any line stating with #
+    int count = 0;
+    char ch;
+    for (ch = getc(file); ch!= EOF; ch = getc(file))
+    {
+        switch (ch)
+        {
+        case '\n':count++;break;
+        default:break;
+        }
+    }
+    //subtract one from the counter because we must skip the first line as it is a comment
+    count--;
+    return count;
+}
+void ReadProcessesData(FILE* file, struct Process Processes[],int ProcessesNum)
+{
+    //note that the pointer of the file points to the end of it
+    //because we have count the number of processes so we need to move it again to the begining of the file
+    fseek(file,0,SEEK_SET);
+    //skip the first line because it is a comment
+    char dummy[10];
+    for (int i=0;i<4;i++)
+    {
+        fscanf(file,"%s",dummy);
+    }
+    for (int i=0;i<ProcessesNum;i++)
+    {
+        fscanf(file,"%d",&Processes[i].ProcessData[0]);
+        fscanf(file,"%d",&Processes[i].ProcessData[1]);
+        fscanf(file,"%d",&Processes[i].ProcessData[2]);
+        fscanf(file,"%d",&Processes[i].ProcessData[3]);
+    }
+}
+void Create_Scheduler_Clk(int& sch_pid, int& clk_pid)
+{
+    sch_pid = fork();
+    if (sch_pid == -1)
+    {
+        perror("error has been occured while initiating the scheduler\n");
+        exit(-1);
+    }
+    if (sch_pid == 0)
+    {
+        execl("./scheduler.out","./scheduler.out",NULL);
+    }
+    clk_pid = fork();
+    if (clk_pid == -1)
+    {
+        perror("error has been occured while initiating the clock\n");
+        exit(-1);
+    }
+    if (clk_pid == 0)
+    {
+        execl("./clk.out","./clk.out",NULL);
+    }
 }
