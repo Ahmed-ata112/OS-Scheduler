528e324cadfd4f7d899ad3251616abe6f8a34559
Sarah Elzayat <sarahelzayat@outlook.com>
2022-04-29 15:57:44 +0200

    PQ

--- code/priority_queue.c
+++ code/priority_queue.c
@@ -1,11 +1,9 @@
-#include "priority_queue.h"
-
+#include "priorityQueue.h"
 
 /*
     Function to initialize the min heap with size = 0
 */
-minHeap initMinHeap()
-{
+minHeap initMinHeap() {
     minHeap hp;
     hp.size = 0;
     return hp;
@@ -15,8 +13,7 @@ minHeap initMinHeap()
 /*
     Function to swap priority within two nodes of the min heap using pointers
 */
-void swap(node *n1, node *n2)
-{
+void swap(node* n1, node* n2) {
     node temp = *n1;
     *n1 = *n2;
     *n2 = temp;
@@ -29,33 +26,27 @@ void swap(node *n1, node *n2)
     may be violated. In such cases, heapify function can be called to make sure that
     heap property is never violated
 */
-void heapify(minHeap *hp, int i)
-{
+void heapify(minHeap* hp, int i) {
     int smallest = (LCHILD(i) < hp->size && hp->elem[LCHILD(i)].priority < hp->elem[i].priority) ? LCHILD(i) : i;
-    if (RCHILD(i) < hp->size && hp->elem[RCHILD(i)].priority < hp->elem[smallest].priority)
-    {
+    if (RCHILD(i) < hp->size && hp->elem[RCHILD(i)].priority < hp->elem[smallest].priority) {
         smallest = RCHILD(i);
     }
-
-    if (smallest != i)
-    {
+    if (smallest != i) {
         swap(&(hp->elem[i]), &(hp->elem[smallest]));
         heapify(hp, smallest);
     }
 }
 
+
 /*
     Function to insert a node into the min heap, by allocating space for that node in the
     heap and also making sure that the heap property and shape propety are never violated.
 */
-void push(minHeap *hp, int priority, int data)
-{
-    if (hp->size)
-    {
+void push(minHeap* hp, int priority, int data) {
+    if (hp->size) {
         hp->elem = realloc(hp->elem, (hp->size + 1) * sizeof(node));
     }
-    else
-    {
+    else {
         hp->elem = malloc(sizeof(node));
     }
 
@@ -64,18 +55,14 @@ void push(minHeap *hp, int priority, int data)
     nd.data = data;
 
     int i = (hp->size)++;
-    while (i && nd.priority < hp->elem[PARENT(i)].priority)
-    {
-        if(nd.priority == hp->elem[PARENT(i)].priority)
-    break;
+    while (i && nd.priority < hp->elem[PARENT(i)].priority) {
         hp->elem[i] = hp->elem[PARENT(i)];
         i = PARENT(i);
     }
     hp->elem[i] = nd;
-
 }
 
-int isEmpty(minHeap *hp)
+int isEmpty(minHeap* hp)
 {
     return hp->size == 0;
 }
@@ -85,13 +72,10 @@ int isEmpty(minHeap *hp)
     and then call heapify function to make sure that the heap property
     is never violated
 */
-node *pop(minHeap *hp)
-{
-    if (hp->size)
-    {
-
+node* pop(minHeap* hp) {
+    if (hp->size) {
         struct node n;
-        struct node *temp = &n;
+        struct node* temp = &n;
         temp->data = hp->elem[0].data;
         temp->priority = hp->elem[0].priority;
         hp->elem[0] = hp->elem[--(hp->size)];
@@ -99,19 +83,17 @@ node *pop(minHeap *hp)
         heapify(hp, 0);
         return temp;
     }
-    else
-    {
+    else {
         free(hp->elem);
         return NULL;
     }
 }
 
-struct node *peek(minHeap *hp)
+struct node* peek(minHeap* hp)
 {
-    if (hp->size)
-    {
+    if (hp->size) {
         struct node n;
-        struct node *temp = &n;
+        struct node* temp = &n;
         temp->data = hp->elem[0].data;
         temp->priority = hp->elem[0].priority;
         return temp;
